4f108d838a399440e184cf09de5c36868f3f1ed0
OrionK08 <orionkastor8@gmail.com>
2025-05-20 07:33:41 -0500

    Directiva ngClass

--- Angular-Dev/src/app/app.component.ts
+++ Angular-Dev/src/app/app.component.ts
@@ -15,4 +15,16 @@ export class AppComponent {
   onSayHello(message : any) {
     console.log(message);
   }
+
+  // ! Esta funcion recibe un string y devuelve un objeto con las clases css a mode de clave
+  // ! y un booleano como valor
+  updateNameClass(name: string) {
+    return {
+      'error' : name.length <= 3,
+      'warning' : name.length > 3 && name.length <= 6,
+      'success' : name.length > 6,
+      'bold' : name.length > 8
+    }
+
+  }
 }
